acb9414c06ff6db5fe7d1d31a0601587689c06f4
trgkpc <trgkpc@gmail.com>
2021-04-30 14:34:10 +0900

    LEDクラスのバグを修正

--- firmware/LED.py
+++ firmware/LED.py
@@ -6,24 +6,23 @@ import colorsys as cs
 class LED:
     def __init__(self, red, green, blue):
         self.leds = [red, green, blue]
-        pi.pinMode(led, pi.OUTPUT)
-        for led in leds:
+        pi.wiringPiSetupGpio()
+        for led in self.leds:
+            pi.pinMode(led, pi.OUTPUT)
             pi.softPwmCreate(led, 0, 100)
             pi.softPwmWrite(led, 0)
-        GPIO.setup(12, GPIO.OUT)
 
-    def impl(self, r, g, b):
-        pi.softPwmWrite(red, int(r))
-        pi.softPwmWrite(green, int(g))
-        pi.softPwmWrite(blue, int(b))
+    def impl(self, rgb):
+        for (led, pulse) in zip(self.leds, rgb):
+            pi.softPwmWrite(led, int(pulse))
 
     def __call__(self, r, g, b):
         # rgbをそれぞれ0.0~1.0で渡す
-        self.softPwmWrite(red, 100*r, 100*g, 100*b)
+        self.impl([100*r, 100*g, 100*b])
 
 
 if __name__ == '__main__':
-    led = LED(21, 20, 16)
+    led = LED(21, 26, 16)
     led(0.5, 0.3, 0.0)
     time.sleep(1.0)
 
