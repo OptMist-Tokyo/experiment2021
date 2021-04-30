ca5a9ee465e2c381131b404bb3d88d9d96562036
trgkpc <trgkpc@gmail.com>
2021-04-30 15:02:29 +0900

    sample ledを変更

--- software/sample_led.py
+++ software/sample_led.py
@@ -10,11 +10,13 @@ from LED import LED
 
 if __name__ == '__main__':
     led1 = LED(17, 27, 22)
+    led1(255, 255, 255)
+    time.sleep(1.0)
 
     while True:
         N = 60
         T = 1.0
         for i in range(N):
-            led1.setRGB(cs.hsv_to_rgb(i/N, 0.5, 0.3))
+            led1.setRGB([255*x for x in cs.hsv_to_rgb(i/N, 0.5, 0.3)])
             time.sleep(T/N)
 
