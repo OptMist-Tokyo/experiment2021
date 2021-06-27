afcdeeb47fc8d894d427d3f749cd41196a0ae28a
trgkpc <trgkpc@gmail.com>
2021-06-27 21:43:31 +0900

    操作後のインターフェース改善

--- firmware/IoT.py
+++ firmware/IoT.py
@@ -6,18 +6,20 @@ import subprocess as sp
 app = Flask(__name__)
 port = 8081
 
+ip = '127.0.0.1'
+page = f'<a href= "http://{ip}:{port}/led/000000">消灯</a> <br>\n\
+<a href="http://{ip}:{port}/led/ffffff">白</a><br>\n\
+<a href="http://{ip}:{port}/led/ff00000">赤</a><br>\n\
+<a href="http://{ip}:{port}/led/00ff00">緑</a><br>\n\
+<a href="http://{ip}:{port}/led/0000ff">青</a><br>\n\
+<a href="http://{ip}:{port}/led/6f00ff">紫</a><br>\n\
+<a href="http://{ip}:{port}/led/ffd400">黄</a><br>\n\
+<a href="http://{ip}:{port}/led/afdfe4">水</a><br>\n\
+<a href="http://{ip}:{port}/led/f15a22">橙</a><br>'
+
 @app.route('/')
 def index():
-    ip = '127.0.0.1'
-    return f'<a href= "http://{ip}:{port}/led/000000">消灯</a> <br>\n\
-    <a href="http://{ip}:{port}/led/ffffff">白</a><br>\n\
-    <a href="http://{ip}:{port}/led/ff00000">赤</a><br>\n\
-    <a href="http://{ip}:{port}/led/00ff00">緑</a><br>\n\
-    <a href="http://{ip}:{port}/led/0000ff">青</a><br>\n\
-    <a href="http://{ip}:{port}/led/6f00ff">紫</a><br>\n\
-    <a href="http://{ip}:{port}/led/ffd400">黄</a><br>\n\
-    <a href="http://{ip}:{port}/led/afdfe4">水</a><br>\n\
-    <a href="http://{ip}:{port}/led/f15a22">橙</a><br>'
+    return page
 
 @ app.route('/led/<RGB>')
 def led(RGB='0xffffff'):
@@ -27,7 +29,7 @@ def led(RGB='0xffffff'):
     cmd = ' '.join(["python3", "Client.py", str(red), str(green), str(blue)])
     sp.run(cmd, shell=True)    
     print(cmd)
-    return jsonify({"ok": True, "red":red, "green":green, "blue":blue}), 200
+    return page
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=False)
